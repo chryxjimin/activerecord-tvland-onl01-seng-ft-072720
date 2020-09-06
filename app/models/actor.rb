859a8a2ebfb9d2589a5de357f0ef1092e6d989f1
Christie Yoon <cleex1999@gmail.com>
2020-09-06 22:57:52 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -8,6 +8,6 @@ class Actor < ActiveRecord::Base
 
    def list_roles
      binding.pry
-     self.characters.map {|character| "#{character.name}  #{character.show}"}.join
+         self.characters.collect {|character| "#{character.name} - #{character.show.name}"}.join(", ")
    end
 end
